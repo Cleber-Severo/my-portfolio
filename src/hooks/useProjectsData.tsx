561526f23ff765b616f4e15f0fad0be500e1985e
Cléber Severo <clebersevero1998@gmail.com>
2024-12-22 20:17:15 -0300

    feat: add finaces project obj

--- src/hooks/useProjectsData.tsx
+++ src/hooks/useProjectsData.tsx
@@ -2,6 +2,7 @@ import { SiReact } from '@react-icons/all-files/si/SiReact';
 import { FaJs } from '@react-icons/all-files/fa/FaJs';
 import { FaCss3Alt } from '@react-icons/all-files/fa/FaCss3Alt';
 import { FaSass } from '@react-icons/all-files/fa/FaSass';
+import { FaHtml5 } from '@react-icons/all-files/fa/FaHtml5';
 
 const useProjectsData = () => {
   const listProjects = [
@@ -37,16 +38,16 @@ const useProjectsData = () => {
     },
     {
       id: 3,
-      title: 'Meteora',
-      description: 'Loja virtual interativa',
-      url: 'https://meteora-9lmj.vercel.app/',
-      githubUrl: 'https://github.com/Cleber-Severo/Meteora',
-      img: 'images/projeto-meteora.png',
+      title: 'Controle de Despesas',
+      description: 'Aplicativo de controle financeiro',
+      url: 'https://cleber-severo.github.io/Controle-de-despesas/',
+      githubUrl: 'https://github.com/Cleber-Severo/Controle-de-despesas',
+      img: 'images/controle-despesas.png',
       icons: (
         <>
-          <SiReact />
-          <FaJs />
           <FaCss3Alt />
+          <FaHtml5 />
+          <FaJs />
         </>
       ),
     },
