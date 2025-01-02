9aa18aeab1d3f2cdbd5474dd7cc488cd8150cbc0
Cléber Severo <clebersevero1998@gmail.com>
2025-01-02 14:11:16 -0300

    style: footer css file and responsive layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,11 @@
+import './styles/Footer.css'
+
 const Footer = () => {
   const date = new Date();
   const currentYear = date.getFullYear();
 
   return (
-    <footer
-      style={{
-        color: '#d9d9d9',
-        borderTop: '1px solid rgba(255,255,255, 0.2)',
-        display: 'flex',
-        justifyContent: 'space-between',
-        padding: '1.5rem 1rem',
-        width: '100%',
-      }}
-    >
+    <footer>
       <h3>&copy; {currentYear} Todos Direitos Reservados</h3>
       <h3>Desenvolvido por: Cléber Severo</h3>
     </footer>
